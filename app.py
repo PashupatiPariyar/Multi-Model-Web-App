6561c465ab6464ebdfe63f448e2521e193e834be
Nitin Kumar Chetwani <hnmn3.nitin@gmail.com>
2020-06-04 14:09:17 +0530

    Fixed issues

--- app.py
+++ app.py
@@ -1,29 +1,29 @@
-from flask import Flask, request, jsonify, render_template
 import pickle
 
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 model = pickle.load(open('model.pkl', 'rb'))
 
+
 @app.route('/')
 def home():
-	return render_template('home.html')
+    return render_template('home.html')
+
 
 @app.route('/height_weight', methods=['GET', 'POST'])
 def predict_weight():
+    if request.method == 'POST':
+        height = float(request.form['height'])
+        gender = float(request.form['gender'])
+        prediction = model.predict([[gender, height]])
 
-	global weights
+        weights = round(prediction[0], 2)
 
-	if request.method == 'POST':
-		height = float(request.form['height'])
-		gender = float(request.form['gender'])
+        return render_template('height_weight.html', weights='Your Weight is: {}'.format(weights))
+    return render_template('height_weight.html')
 
-		prediction = model.predict([[gender, height]])
 
-		weights = round(prediction[0], 2)
-
-		return render_template('height_weight.html', weights='Your Weight is: {}'.format(weights))
-
-		if __name__ == "__main__":
-			app.run()
+if __name__ == "__main__":
+    app.run()
